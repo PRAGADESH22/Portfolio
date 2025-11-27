541da1115dabf279686ab1a2b445c4893f539cf0
PRAGADESH22 <pragadeshpragadesh00@gmail.com>
2025-11-27 23:10:48 +0530

    media queries newly added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.jpg";
 import line from "../../assets/line.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu.png";
+import close from "../../assets/close.png";
+import logoP from "../../assets/letter-p.png";
 
 const Navbar = () => {
+  const menuRef = useRef();
+  let openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  let closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
+
   let [menu, setmenu] = useState(" ");
+  // useEffect(() => {
+  //   if (menuRef.current) {
+  //     const rect = menuRef.current.getBoundingClientRect();
+  //   }
+  // }, []);
   return (
-    <div className="navbar">
+    <div className="navbar" id="home">
       <img src={logo} alt="" className="nav-logo" />
-      <ul className="nav-menu">
+      <div className="imgage-wrap">
+        <img
+          src={logoP}
+          alt=""
+          style={{ width: "48px", height: "48px" }}
+          className="navbar-logoP"
+        />
+        <h2>ragadesh</h2>
+      </div>
+      <img
+        src={menu_open}
+        alt=""
+        style={{ width: "32px", height: "32px" }}
+        className="navbar-menu-open"
+        onClick={openMenu}
+      />
+
+      <ul className="nav-menu" ref={menuRef}>
+        <img
+          src={close}
+          alt=""
+          style={{ width: "32px", height: "32px" }}
+          className="navbar-menu-close"
+          onClick={closeMenu}
+        />
         <li>
           <AnchorLink href="#home" offset={50} className="anchor-link">
             <p onClick={() => setmenu("Home")}>Home</p>
@@ -66,7 +106,11 @@ const Navbar = () => {
           )}
         </li>
       </ul>
-      <div className="nav-connect"><AnchorLink href="#contact" offset={50} className="anchor-link">Connect With Me</AnchorLink></div>
+      <div className="nav-connect">
+        <AnchorLink href="#contact" offset={50} className="anchor-link">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
   );
 };
